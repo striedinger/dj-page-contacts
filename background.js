b8465af3566ade2266a8512e9f999118fbe6bd00
Hugo Striedinger <striedinger@outlook.com>
2021-03-19 15:51:22 -0400

    save page json to memory to avoid file lock

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 chrome.runtime.onInstalled.addListener(async () => {
-  const pages = await fetch(chrome.runtime.getURL('src/data/pages.json')).then(response => response.json()).then(json => json);
+  const pagesJson = await fetch(chrome.runtime.getURL('src/data/pages.json')).then(response => response.json()).then(json => json);
+  const pages = [...pagesJson];
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: pages.map(page => {
